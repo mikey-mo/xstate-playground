3709b7282cee24892041361833ffc725c902e2e6
Michael Monahan <mike@palla.app>
2021-10-28 23:26:07 -0500

    woo lad

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,9 @@
 import '../styles/globals.css'
+import { inspect } from '@xstate/inspect';
+
+if (typeof window !== 'undefined') {
+  inspect({ iframe: false });
+}
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
